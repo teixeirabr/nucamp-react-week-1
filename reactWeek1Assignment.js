54338971f708a08e0a40418872c59da6e6f4c8ec
andre <andrelaprov@gmail.com>
2021-12-07 23:58:07 -0500

    added student and bootcamp classes

---
+++ reactWeek1Assignment.js
@@ -0,0 +1,15 @@
+class Student {
+  constructor(name, email, community) {
+    this.name = name;
+    this.email = email;
+    this.community = community;
+  }
+}
+
+class Bootcamp {
+  constructor(name, level, students = []) {
+    this.name = name;
+    this.level = level;
+    this.students = students;
+  }
+}
